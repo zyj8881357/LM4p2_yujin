122263d08ee98d9c96413959c77b51387a70bfe0
Sergey Malyshev <Sergey.Malyshev@noaa.gov>
2020-01-27 21:14:58 -0500

    Add initialization of surface resitance and litter resistance calculations
    also simplify arguments to resistance calculating functions

--- surface_resistance.F90
+++ surface_resistance.F90
@@ -1,10 +1,22 @@
 module surface_resistance_mod
 ! calculations of soil resistance to water vapor and heat
+
+#ifdef INTERNAL_FILE_NML
+use mpp_mod, only: input_nml_file
+#else
+use fms_mod, only: open_namelist_file
+#endif
+
 use constants_mod, only : dens_h2o, pi, grav
+use fms_mod, only: error_mesg, NOTE, WARNING, FATAL, file_exist, &
+     close_file, check_nml_error, stdlog, string, lowercase
+use mpp_mod, only: mpp_pe, mpp_root_pe
 use land_constants_mod, only : diffusivity_h2o, kin_visc_air
+use land_data_mod, only : log_version
 use land_numerics_mod, only : gamma
-! use vegn_tile_mod, only: vegn_tile_type
-use soil_tile_mod, only: soil_tile_type, soil_data_hydraulic_properties, dz
+use vegn_tile_mod, only: vegn_tile_type, vegn_tile_bwood, vegn_tile_LAI, vegn_tile_SAI
+use soil_tile_mod, only: soil_tile_type, soil_data_hydraulic_properties, dz, &
+        get_soil_litter_C
 
 implicit none
 private
@@ -12,28 +24,107 @@ private
 ! ==== public interfaces =====================================================
 public :: surface_resistance_init
 public :: surface_resistance_end
+
+public :: evap_resistance_litter
+public :: evap_resistance_soil
+
 public :: soil_evap_sv_resistance
 public :: soil_evap_bl_resistance
 public :: sfc_visc_bl_depth
 ! ==== end of public interfaces ==============================================
 
 ! ==== module constants ======================================================
-character(len=*), parameter :: module_name = 'soil_resistance'
+character(len=*), parameter :: module_name = 'surface_resistance_mod'
 #include "../shared/version_variable.inc"
 
 ! ==== module variables ======================================================
 logical :: module_is_initialized = .FALSE.
 
+logical :: do_evap_resiatance_soil = .FALSE. ! if TRUE, evaporation is slowed down by
+  ! resistances in soil upper layer and viscous sublayer
+real :: rav_lit_0         = 0.0 ! constant litter resistance to vapor
+real :: rav_lit_vi        = 0.0 ! litter resistance to vapor per LAI+SAI
+real :: rav_lit_fsc       = 0.0 ! litter resistance to vapor per fsc
+real :: rav_lit_ssc       = 0.0 ! litter resistance to vapor per ssc
+real :: rav_lit_deadmic   = 0.0 ! litter resistance to vapor per dead microbe C
+real :: rav_lit_bwood     = 0.0 ! litter resistance to vapor per bwood
+
+namelist /surface_resistance_nml/ &
+    do_evap_resiatance_soil, &
+    rav_lit_0, rav_lit_vi, rav_lit_fsc, rav_lit_ssc, rav_lit_deadmic, rav_lit_bwood
+
 contains ! -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
+! ---------------------------------------------------------------------------------------
 subroutine surface_resistance_init()
-   ! read namelist
-   module_is_initialized = .TRUE.
+  integer :: unit, io, iostat, ierr
+  call log_version(version, module_name, &
+  __FILE__)
+#ifdef INTERNAL_FILE_NML
+    read (input_nml_file, nml=surface_resistance_nml, iostat=io)
+    ierr = check_nml_error(io, 'surface_resistance_nml')
+#else
+  if (file_exist('input.nml')) then
+     unit = open_namelist_file()
+     ierr = 1;
+     do while (ierr /= 0)
+        read (unit, nml=surface_resistance_nml, iostat=io, end=10)
+        ierr = check_nml_error (io, 'surface_resistance_nml')
+     enddo
+10   continue
+     call close_file (unit)
+  endif
+#endif
+
+  unit=stdlog()
+
+  if (mpp_pe() == mpp_root_pe()) then
+     write(unit, nml=surface_resistance_nml)
+  endif
+
+  module_is_initialized = .TRUE.
 end subroutine surface_resistance_init
 
+! ---------------------------------------------------------------------------------------
 subroutine surface_resistance_end()
   ! do nothing
 end subroutine surface_resistance_end
+
+! ---------------------------------------------------------------------------------------
+! additional resistance of litter to the water vapor flux.
+! not a good parameterization, but just using for sensitivity analyses now.
+! ignores differing biomass and litter turnover rates.
+real function evap_resistance_litter(soil,vegn) result(rav_lit)
+  type(soil_tile_type), intent(in) :: soil
+  type(vegn_tile_type), intent(in) :: vegn
+
+  real :: litter_fast_C, litter_slow_C, litter_deadmic_C ! litter carbon pools
+
+  call get_soil_litter_C(soil, litter_fast_C, litter_slow_C, litter_deadmic_C)
+
+  associate(cc=>vegn%cohorts)
+  rav_lit = rav_lit_0 + rav_lit_vi * (vegn_tile_LAI(vegn)+vegn_tile_SAI(vegn)) &
+                      + rav_lit_fsc * litter_fast_C &
+                      + rav_lit_ssc * litter_slow_C &
+                      + rav_lit_deadmic * litter_deadmic_C &
+                      + rav_lit_bwood * vegn_tile_bwood(vegn)
+  end associate
+end function evap_resistance_litter
+
+! ---------------------------------------------------------------------------------------
+real function evap_resistance_soil(soil,u_sfc,ustar_sfc,p) result(res)
+  type(soil_tile_type), intent(in) :: soil
+  real, intent(in) :: u_sfc     ! near-surface wind velocity, m/s
+  real, intent(in) :: ustar_sfc ! friction velocity at the soil surface, m/s
+  real, intent(in) :: p         ! surface pressure, N/m2
+
+  if (do_evap_resiatance_soil) then
+     res = soil_evap_sv_resistance(soil) &
+         + soil_evap_bl_resistance(soil, u_sfc, ustar_sfc, p)
+  else
+     res = 0.0
+  endif
+end function evap_resistance_soil
 
 ! ---------------------------------------------------------------------------------------
 ! internal soil viscous resistance to evaporation.
@@ -65,11 +156,10 @@ end function soil_evap_sv_resistance
 ! Haghighi and Or (2013): Evaporation from porous surfaces into turbulent airflows: Coupling
 !      eddy characteristics with pore scale vapor diffusion. Water Resources Research, 49,
 !      8432–8442, doi:10.1002/2012WR013324View.
-real function soil_evap_bl_resistance(soil, u_sfc, ustar_sfc, T, p) result(r_bl)
+real function soil_evap_bl_resistance(soil, u_sfc, ustar_sfc, p) result(r_bl)
    type(soil_tile_type), intent(in) :: soil
    real, intent(in) :: u_sfc     ! near-surface wind velocity, m/s
    real, intent(in) :: ustar_sfc ! friction velocity at the soil surface, m/s
-   real, intent(in) :: T         ! temperature, degK
    real, intent(in) :: p         ! pressure, N/m2
 
    real, parameter :: sfc_tension_h2o = 0.071 ! surface tension of liquid water, J/m2
@@ -80,13 +170,15 @@ real function soil_evap_bl_resistance(soil, u_sfc, ustar_sfc, T, p) result(r_bl)
    real :: psi_sat_sfc ! saturated matric water potential at the surface, m
    real :: theta_sfc ! relative soil wetness at the surface, unitless
    real :: r_pores  ! surface pore radius, m
+   real :: T_sfc    ! temperature, degK
 
    theta_sfc = max(0.0, soil%wl(1) / (dens_h2o * dz(1)))/soil%pars%vwc_sat
+   T_sfc = soil%T(1)
    f_diff = (sqrt(pi/theta_sfc)-1)/(pi*sqrt(theta_sfc)) ! surface wetness dependency model, Schlunder (1988)
-   d_bl = sfc_visc_bl_depth(u_sfc, ustar_sfc, T, p) ! depth of viscous sublayer
+   d_bl = sfc_visc_bl_depth(u_sfc, ustar_sfc, T_sfc, p) ! depth of viscous sublayer
    psi_sat_sfc = abs(soil%pars%psi_sat_ref/soil%alpha(1)) ! saturated matric water potential at the surface, m
    r_pores = 2*sfc_tension_h2o/(dens_h2o*grav*psi_sat_sfc)
-   r_bl = (d_bl + r_pores*f_diff)/diffusivity_h2o(T,p)
+   r_bl = (d_bl + r_pores*f_diff)/diffusivity_h2o(T_sfc,p)
 end function soil_evap_bl_resistance
 
 ! ---------------------------------------------------------------------------------------
